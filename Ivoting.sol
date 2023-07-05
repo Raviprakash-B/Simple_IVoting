69b8c6bcb9d93d9519b762cbf287a9baae8d1aae
Raviprakash <46168102+Raviprakash-B@users.noreply.github.com>
2023-07-05 13:16:40 +0530

    Update Ivoting.sol

--- Ivoting.sol
+++ Ivoting.sol
@@ -1,1 +1,10 @@
+//SPDX-License-Identifier:GPL-3.0
+pragma solidity >=0.5.0 <=0.9.0;
 
+interface Ivoting{
+    //In tnterface function always should be external
+    //Vote,totalvotes,validcandidate
+    function voteForCandidate(bytes32 candidate) external;
+    function totalvotes(bytes32 candidate) external view returns(uint);
+    function validcandidate(bytes32  candidate) external view returns(bool);
+}
